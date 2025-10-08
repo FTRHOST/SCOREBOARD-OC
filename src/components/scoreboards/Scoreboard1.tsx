211f3c51896db98734ae7f759677eaf336f84ee3
Muhammad Fathir Al Faruq <fathironmy4@gmail.com>
2025-10-08 02:39:48 +0000

    sudah hampir benar, namun masih ada yang kurang pas, foto pertama itu me

--- src/components/scoreboards/Scoreboard1.tsx
+++ src/components/scoreboards/Scoreboard1.tsx
@@ -9,10 +9,18 @@ import Image from 'next/image';
 const Scoreboard1 = () => {
   const { scoreboard, loading } = useScoreboardData();
 
-  if (loading || !scoreboard) {
+  if (loading) {
     return (
-      <div className="w-full max-w-6xl aspect-[1048/227] flex items-center justify-center bg-gray-800 text-white font-display">
+       <div className="w-full max-w-6xl aspect-[1048/227] flex items-center justify-center text-white font-display">
         Loading Scoreboard...
+      </div>
+    );
+  }
+  
+  if (!scoreboard) {
+    return (
+      <div className="w-full max-w-6xl aspect-[1048/227] flex items-center justify-center text-white font-display">
+        Scoreboard data not available.
       </div>
     );
   }
@@ -20,52 +28,57 @@ const Scoreboard1 = () => {
   const { teamAName, teamBName, teamAScore, teamBScore, half, teamAColor, teamBColor, logoSrc } = scoreboard;
 
   return (
-    <div className="w-full max-w-6xl aspect-[1048/227] relative font-display text-white bg-transparent">
-      {/* Team Names Background */}
-      <div className="absolute top-[41px] w-full h-[105px] flex justify-between">
-        <div style={{ backgroundColor: teamAColor }} className="w-[30.5%] h-full"></div>
-        <div style={{ backgroundColor: teamBColor }} className="w-[30.5%] h-full"></div>
-      </div>
-      
-      {/* Central Scoreboard Background */}
-      <div className="absolute top-[41px] left-1/2 -translate-x-1/2 w-[39%] h-[105px] bg-[#05183b]"></div>
-
-      {/* Half Background */}
-      <div className="absolute top-[179px] left-1/2 -translate-x-1/2 w-[22.2%] h-[48px] bg-[#05183b]"></div>
-      
-      {/* Content Layer */}
+    <div className="w-full max-w-6xl mx-auto aspect-[1048/227] relative font-display text-white">
+      {/* Layer 1: Backgrounds */}
       <div className="absolute inset-0">
-        {/* Team Names Text */}
-        <div className="absolute top-[41px] w-full h-[105px] flex justify-between items-center text-[82px] px-[1.5%]">
-            <div className="w-[30.5%] text-center truncate">{teamAName}</div>
-            <div className="w-[30.5%] text-center truncate">{teamBName}</div>
-        </div>
+        {/* Team A Name BG */}
+        <div style={{ backgroundColor: teamAColor }} className="absolute top-[41px] left-0 w-[320px] h-[105px]" />
+        {/* Scoreboard BG */}
+        <div className="absolute top-[41px] left-1/2 -translate-x-1/2 bg-[#05183b] w-[408px] h-[105px]" />
+        {/* Team B Name BG */}
+        <div style={{ backgroundColor: teamBColor }} className="absolute top-[41px] right-0 w-[320px] h-[105px]" />
+        {/* Half BG */}
+        <div className="absolute top-[179px] left-1/2 -translate-x-1/2 bg-[#05183b] w-[233px] h-[48px]" />
+      </div>
 
+      {/* Layer 2: Text Content (Scores, Names, Half) */}
+      <div className="absolute inset-0">
+        {/* Team A Name */}
+        <div className="absolute top-[41px] left-0 w-[320px] h-[103px] flex items-center justify-center text-[82px] truncate px-2">
+          {teamAName}
+        </div>
+        {/* Team B Name */}
+        <div className="absolute top-[41px] right-0 w-[320px] h-[103px] flex items-center justify-center text-[82px] truncate px-2">
+          {teamBName}
+        </div>
         {/* Scores */}
-        <div className="absolute top-[20px] left-1/2 -translate-x-1/2 w-[39%] h-[105px] flex justify-between items-center text-[96px] px-[2.5%]">
-          <AnimatedNumber value={teamAScore} />
-          <AnimatedNumber value={teamBScore} />
+        <div className="absolute top-[41px] left-1/2 -translate-x-1/2 w-[408px] h-[105px] flex items-center justify-between text-[96px] px-[31px]">
+          <div className="absolute top-[-21px] left-[31px]">
+             <AnimatedNumber value={teamAScore} />
+          </div>
+           <div className="absolute top-[-21px] right-[31px]">
+             <AnimatedNumber value={teamBScore} />
+          </div>
         </div>
-
         {/* Half Text */}
-        <div className="absolute top-[179px] left-1/2 -translate-x-1/2 w-[22.2%] h-[48px] flex items-center justify-center text-[34px]">
+        <div className="absolute top-[179px] left-1/2 -translate-x-1/2 w-[233px] h-[48px] flex items-center justify-center text-[34px]">
           {half}
         </div>
-        
-        {/* Logo */}
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[22.7%] h-[82.8%]">
-          <div className="relative w-full h-full">
-            {logoSrc ? (
+      </div>
+      
+      {/* Layer 3: Logo (On Top) */}
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[238px] h-[188px]">
+         <div className="relative w-full h-full">
+           {logoSrc ? (
               <Image 
                 src={logoSrc} 
                 alt="Uploaded Logo" 
-                layout="fill"
-                objectFit="contain"
+                fill
+                style={{objectFit: "contain"}}
               />
             ) : (
               <OsisCupLogo className="w-full h-full text-white" />
             )}
-          </div>
         </div>
       </div>
     </div>
